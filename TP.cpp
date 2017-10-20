f0d2addb336503e3236d11115884e39e9c4f98f7
fconejero <facu.conejero@hotmail.com>
2017-10-20 11:48:10 -0300

    Acá está, la puta que te parió
    Anda hijo de puta

--- TP.cpp
+++ TP.cpp
@@ -3,83 +3,83 @@
 #include <string.h>
 using namespace std;
 
-struct partidos {
+struct partidos{
 	char siglas[5];
 	float votos;
 	int lista;
 };
+
 struct nodo{
 	partidos info;
-	nodo* sgte;
+	nodo*sgte;
 };
+nodo*K;
 partidos aux;
-void agregarPrincipio (nodo*&L, partidos x);
-void insertarOrdenado (nodo*&L, partidos x);
-nodo* buscar (nodo*&C, int d);
-nodo*C;
-nodo*ret=C;
+FILE*PASO;
+
+void insertarOrdenado(nodo*&L, partidos x);
+void agregarPrincipio(nodo*&L, partidos v);
+int buscar(nodo*&L, float y);
 
 int main(){
-	FILE*PASO;
-	nodo*L=NULL;
-	PASO=fopen("PASO.dat","wb+");
-	cout << "Ingrese la primer lista, para terminar el ingreso de partidos al archivo, ingrese \"-1\"." << endl;
-	cin >> aux.lista;
-	while (aux.lista!=-1) {
-		cout << "Ingrese las siglas del partido (hasta 5)" << endl;
-		cin >> aux.siglas;
-		cout << "Ingrese la cantidad porcentual de votos." << endl;
-		cin >> aux.votos;
-		fwrite(&aux, sizeof(partidos), 1, PASO);
-		cout << "Ingrese la primer lista, para terminar el ingreso de partidos al archivo, ingrese \"-1\"." << endl;
+	PASO=fopen("paso.dat","wb+");
+	cout << "Ingrese la primer lista, para terminar el ingreso de partidos al archivo, ingrese \"-1\" cuando el programa vuelva a pedirle una lista." << endl;
 		cin >> aux.lista;
-	}
-	rewind(PASO);
-	fread(&aux, sizeof(partidos), 1, PASO);
-	while (!feof(PASO)){
-		if (aux.votos>=1.5) {
-			insertarOrdenado(L, aux);
+		while (aux.lista!=-1) {
+			cout << "Ingrese las siglas del partido (hasta 5)" << endl;
+			cin >> aux.siglas;
+			cout << "Ingrese la cantidad porcentual de votos." << endl;
+			cin >> aux.votos;
+			fwrite(&aux, sizeof(partidos), 1, PASO);
+			cout << "Ingrese la primer lista, para terminar el ingreso de partidos al archivo, ingrese \"-1\"." << endl;
+			cin >> aux.lista;
 		}
+		rewind(PASO);
 		fread(&aux, sizeof(partidos), 1, PASO);
-	}
-	fclose(PASO);
-	ret=buscar(L,51);
-	while (L!=NULL){
-		cout << L->info.lista << endl;
-		L=L->sgte;
-	}
-	cout << ret << endl;
+		while (!feof(PASO)){
+			if (aux.votos>=1.5){
+				insertarOrdenado(K, aux);
+			}
+			fread(&aux, sizeof(partidos), 1, PASO);
+		}
+		int aux3;
+		aux3=buscar(K, 51);
+		cout << "¡La lista " << aux3 << " llegó al 51% de votos!" << endl;
+		while (K!=NULL){
+			cout << "La lista " << K->info.lista << " llegó al menos al 1.5% de votos." << endl;
+			K=K->sgte;
+		}
 }
 
-void insertarOrdenado (nodo*&L, partidos x) {
-	if (L==NULL || x.votos<L->info.votos) {
-		agregarPrincipio (L, x);
+void insertarOrdenado (nodo*&L, partidos x){
+	if (L==NULL || x.lista<L->info.lista){
+		agregarPrincipio(L, x);
 	}
-	else {
-		nodo*Nuevo= new nodo();
-		Nuevo->info=x;
-		Nuevo->sgte= NULL;
+	else{
+		nodo*NUEVO=new nodo();
+		NUEVO->info=x;
+		NUEVO->sgte=NULL;
 		nodo*aux2=L;
 		nodo*ant=L;
-		while (aux2!=NULL && aux2->info.votos<x.votos) {
+		while(aux2!=NULL && aux2->info.lista<x.lista){
 			ant=aux2;
 			aux2=aux2->sgte;
 		}
-		ant->sgte=Nuevo;
-		Nuevo->sgte=aux2;
+		ant->sgte=NUEVO;
+		NUEVO->sgte=aux2;
 	}
-};
+}
+void agregarPrincipio (nodo*&L, partidos v){
+	nodo*NUEVO=new nodo();
+	NUEVO->info=v;
+	NUEVO->sgte=L;
+	L=NUEVO;
+}
 
-void agregarPrincipio (nodo*&L, partidos x) {
-	nodo*Nuevo=new nodo();
-		Nuevo->info=x;
-		Nuevo->sgte=L;
-		L=Nuevo;
-};
-
-nodo* buscar (nodo*&C, int d){
-	while (ret!=NULL && ret->info.votos<d){
-		ret=ret->sgte;
+int buscar(nodo*&L, float y){
+	nodo*aux4=L;
+	while (aux4!=NULL && aux4->info.votos<y){
+		aux4=aux4->sgte;
 	}
-	return ret;
+	return aux4->info.lista;
 }
